030b8e6e78f16a8f32d8be47e59ae8bda263063d
王怡然 <1452372625@qq.com>
2019-01-03 22:13:41 +0800

    Modify the project strcture. Ref #2

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import 'iview/dist/styles/iview.css'
+import Vue from 'vue'
+import App from './App'
+import Login from './components/Login'
+import store from './store'
+import router from './router'
+import iView from 'iview'
+import axios from 'axios'
+
+Vue.use(iView)
+axios.defaults.baseURL="/api"
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App, Login },
+  template: '<App/>'
+})

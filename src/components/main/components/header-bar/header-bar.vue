931205d0c3053f8a696c8b402610942710daa31f
王怡然 <1452372625@qq.com>
2019-01-04 13:05:20 +0800

    Complete the main interface for out project. Ref #15.

--- src/components/main/components/header-bar/header-bar.vue
+++ src/components/main/components/header-bar/header-bar.vue
@@ -1,41 +1,34 @@
 <template>
-  <Menu mode="horizontal" active-name="1" style="height: 65px" >
-    <MenuItem name="1">
-      <Icon type="ios-paper" />
-      内容管理
-    </MenuItem>
-    <MenuItem name="2">
-      <Icon type="ios-people" />
-      用户管理
-    </MenuItem>
-    <Submenu name="3">
-      <template slot="title">
-        <Icon type="ios-stats" />
-        统计分析
-      </template>
-      <MenuGroup title="使用">
-        <MenuItem name="3-1">新增和启动</MenuItem>
-        <MenuItem name="3-2">活跃分析</MenuItem>
-        <MenuItem name="3-3">时段分析</MenuItem>
-      </MenuGroup>
-      <MenuGroup title="留存">
-        <MenuItem name="3-4">用户留存</MenuItem>
-        <MenuItem name="3-5">流失用户</MenuItem>
-      </MenuGroup>
-    </Submenu>
-    <MenuItem name="4">
-      <Icon type="ios-construct" />
-      综合设置
-    </MenuItem>
-  </Menu>
+  <div class="header-bar">
+    <sider-trigger :collapsed="collapsed" icon="md-menu" @on-change="handleCollpasedChange"></sider-trigger>
+    <custom-bread-crumb show-icon style="margin-left: 30px;" :list="breadCrumbList"></custom-bread-crumb>
+    <div class="custom-content-con">
+      <slot></slot>
+    </div>
+  </div>
 </template>
-
 <script>
-    export default {
-        name: "header-bar"
+import siderTrigger from './sider-trigger'
+import customBreadCrumb from './custom-bread-crumb'
+import './header-bar.less'
+export default {
+  name: 'HeaderBar',
+  components: {
+    siderTrigger,
+    customBreadCrumb
+  },
+  props: {
+    collapsed: Boolean
+  },
+  computed: {
+    breadCrumbList () {
+      return this.$store.state.app.breadCrumbList
     }
+  },
+  methods: {
+    handleCollpasedChange (state) {
+      this.$emit('on-coll-change', state)
+    }
+  }
+}
 </script>
-
-<style scoped>
-
-</style>

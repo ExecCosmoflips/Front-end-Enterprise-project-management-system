043300be8ef2a6af426a0ef98cbeb6d8ee2ad6b4
unknown <917479676@qq.com>
2019-01-05 10:11:40 +0800

    Add show department function to super admin. Ref #32

---
+++ src/view/data-analysis/show-department.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <Card>
+      <tables ref="tables" editable searchable search-place="top" v-model="tableData" :columns="columns" @on-delete="handleDelete"/>
+      <Button style="margin: 10px 0;" type="primary" @click="exportExcel">导出为Csv文件</Button>
+    </Card>
+  </div>
+</template>
+
+<script>
+  import Tables from '_c/tables'
+  import { getTableData } from '@/api/data'
+  export default {
+    name: 'tables_page',
+    components: {
+      Tables
+    },
+    data () {
+      return {
+        columns: [
+          {title: '序号', key: 'id', sortable: true},
+          {title: '部门名', key: 'department', editable: true},
+          {title: '负责人', key: 'manager', editable: true},
+          {
+            title: '部门详情',
+            key: 'handle',
+            button: [
+              (h, params, vm) => {
+                return h('Poptip', {
+                  props: {
+                    confirm: true,
+                    title: '离开本页?'
+                  },
+                  on: {
+                    'on-ok': () => {
+                      vm.$emit('on-delete', params)
+                      vm.$emit('input', params.tableData.filter((item, index) => index !== params.row.initRowIndex))
+                    }
+                  }
+                }, [
+                  h('Button', '查看')
+                ])
+              }
+            ]
+
+          }
+
+        ],
+        tableData: []
+      }
+    },
+    methods: {
+      handleDelete (params) {
+        console.log(params)
+      },
+      exportExcel () {
+        this.$refs.tables.exportCsv({
+          filename: `table-${(new Date()).valueOf()}.csv`
+        })
+      }
+    },
+    mounted () {
+      getTableData().then(res => {
+        this.tableData = res.data
+      })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>

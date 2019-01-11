aff890a1c532d36371da80f987ee8fafe45335d8
纳克孜 <965342865@qq.com>
2019-01-12 01:12:39 +0800

    Improve Add-treasurer.vue. Ref #86

--- src/api/super_admin.js
+++ src/api/super_admin.js
@@ -11,5 +11,24 @@ export const getAAddDepartmentAdmin = (form) => {
   })
 }
 
+export const getAAddTreasurer = (form) => {
+  console.log(form)
+
+  return axios.request({
+    url:'/add_treasurer',
+    data:qs.stringify(form),
+    method:'post'
+  })
+}
+export const SetuppDepartmentName = (form) => {
+  console.log(form)
+
+  return axios.request({
+    url:'/setup_dep_name',
+    data:qs.stringify(form),
+    method:'post'
+  })
+}
+
 
 

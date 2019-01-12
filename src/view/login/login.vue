e3ec15631b8a7074ac86c0fceccb5c0d66e2f1d6
张敏 <841956630@qq.com>
2019-01-12 15:28:19 +0800

    Update update login in view. Ref #92

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -5,10 +5,10 @@
 <template>
   <div class="login">
     <div class="login-con">
-      <Card icon="log-in" title="欢迎登录" :bordered="false">
+      <Card icon="log-in" title="欢迎登录企业项目数据财务管理" :bordered="false">
         <div class="form-con">
           <login-form @on-success-valid="handleSubmit"></login-form>
-          <p class="login-tip">输入任意用户名和密码即可</p>
+          <p class="login-tip">请用户名和密码</p>
         </div>
       </Card>
     </div>

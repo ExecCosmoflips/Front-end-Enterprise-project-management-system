c46bda75e3f5b58535ea8374dbfd1eea463eb0e5
纳克孜 <965342865@qq.com>
2019-01-12 15:19:09 +0800

    Improve SuperAdmin. Ref #94

--- src/view/SuperAdmin/Add-treasurer/Add-treasurer.vue
+++ src/view/SuperAdmin/Add-treasurer/Add-treasurer.vue
@@ -1,77 +1,54 @@
 <template>
-    <i-form v-ref:form-validate :model="formValidate" :rules="ruleValidate" :label-width="80">
-        <Form-item label="工号" prop="username">
-           <i-input :value.sync="formValidate.username" placeholder="请输入工号"></i-input>
-        </Form-item>
-        <Form-item label="姓名" prop="name">
-            <i-input :value.sync="formValidate.name" placeholder="请输入姓名"></i-input>
-        </Form-item>
-        <Form-item label="邮箱" prop="email">
-            <i-input :value.sync="formValidate.email" placeholder="请输入邮箱"></i-input>
-        </Form-item>
-        <Form-item label="手机号" prop="phone">
-             <i-input :value.sync="formValidate.phone" placeholder="请输入手机号"></i-input>
-        </Form-item>
-       <FormItem label="性别" prop="gender">
-             <RadioGroup v-model="formItem.radio">
-               <Radio label="male">男</Radio>
-               <Radio label="female">女</Radio>
-             </RadioGroup>
-           </FormItem>
-        <Form-item>
-            <i-button type="primary" @click="handleSubmit('formValidate')">添加</i-button>
-            <i-button type="ghost" @click="handleReset('formValidate')" style="margin-left: 8px">重置</i-button>
-        </Form-item>
-    </i-form>
+  <Form :model="formItem" :label-width="80">
+
+    <FormItem label="工号" aria-setsize="10">
+      <Input v-model="formItem.username" placeholder="请输入工号"></Input>
+    </FormItem>
+    <FormItem label="姓名" aria-setsize="10">
+      <Input v-model="formItem.name" placeholder="请输入姓名"></Input>
+    </FormItem>
+    <FormItem label="邮箱" aria-setsize="10">
+      <Input v-model="formItem.email" placeholder="请输入邮箱"></Input>
+    </FormItem>
+    <FormItem label="手机号" aria-setsize="10">
+      <Input v-model="formItem.phone" placeholder="请输入手机号"></Input>
+    </FormItem>
+    <FormItem label="性别">
+      <RadioGroup v-model="formItem.gender">
+        <Radio label="male">男</Radio>
+        <Radio label="female">女</Radio>
+      </RadioGroup>
+    </FormItem>
+    <FormItem>
+      <Button type="primary" @click="submit(formItem)">submit</Button>
+    </FormItem>
+  </Form>
 </template>
-<script>
+<script>import {state, mapActions } from 'vuex'
 export default {
-  name: 'SuperAdmin',
-  data () {
+  name: 'Add-treasurer',
+  data() {
     return {
       formItem: {
-        radio: ''
-      },
-      formValidate: {
         username: '',
         name: '',
         email: '',
         gender: '',
         phone: ''
-      },
-      ruleValidate: {
-        username: [
-          { required: true, message: '工号不能为空', trigger: 'blur' }
-        ],
-        name: [
-          { required: true, message: '姓名不能为空', trigger: 'blur' }
-        ],
-        email: [
-          { required: true, message: '邮箱不能为空', trigger: 'blur' },
-          { type: 'email', message: '邮箱格式不正确', trigger: 'blur' }
-        ],
-        phone: [
-          { required: true, message: '手机号不能为空', trigger: 'blur' }
-        ],
-        gender: [
-          { required: true, message: '请选择性别', trigger: 'change' }
-        ]
       }
     }
   },
   methods: {
-    handleSubmit (name) {
-      this.$refs[name].validate((valid) => {
-        if (valid) {
-          this.$Message.success('提交成功!')
-        } else {
-          this.$Message.error('表单验证失败!')
-        }
-      })
+    ...mapActions([
+        'getAddTreasurer'
+      ]
+    ),
+    submit(formItem) {
+      console.log(this.formItem)
+      this.getAddTreasurer(formItem)
+      console.log(formItem)
     },
-    handleReset (name) {
-      this.$refs[name].resetFields()
-    }
   }
 }
+
 </script>

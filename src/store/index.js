ce41593cba05c32105edc17341a77276ab6ae276
陈智 <917479676@qq.com>
2019-01-12 21:51:14 +0800

    Add config to index.js. Ref #105

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,11 @@ import Vuex from 'vuex'
 import user from './module/user'
 import app from './module/app'
 import department from './module/department'
+
 import addreceivablee from './module/addreceivablee'
 import super_admin from './module/super_admin'
+import superadmin from './module/superadmin'
 import data from './module/data'
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -22,9 +23,13 @@ export default new Vuex.Store({
   modules: {
     user,
     app,
+
+
     department,
     addreceivablee,
     super_admin,
-    data
+    data,
+    superadmin
+
   }
 })

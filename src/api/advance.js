d7ec4c03af280ee07f864a0210e156fc1e0653f5
zhushiwen <467170016@qq.com>
2019-01-13 19:52:44 +0800

    Update advance in api file for two api functions. Ref #110

--- src/api/advance.js
+++ src/api/advance.js
@@ -16,3 +16,20 @@ export const getProjectList = (department_id) => {
     method: 'get'
   })
 }
+
+export const getReceivableList = (project_id) => {
+  return axios.request({
+    url: '/get_receivable_list_for_advance',
+    params: {
+      project_id
+    },
+    method: 'get'
+  })
+}
+
+export const listAdvanceInfo = () => {
+  return axios.request({
+    url: '/list_advance_info',
+    method: 'get'
+  })
+}

9938ae63d180c6d795ac5bec22348c3340f4f68a
张敏 <841956630@qq.com>
2019-01-14 21:37:10 +0800

    Add add addexpend in module. Ref #127

---
+++ src/store/module/addexpend.js
@@ -0,0 +1,48 @@
+import {
+  Addexpend,
+  putCategoryList
+} from '../../api/addexpend'
+
+
+export default {
+  state: {
+    categoryList: [],
+    project: '',
+    category: '',
+    title: '',
+    number: '',
+    agreement: ''
+
+  },
+  mutations: {
+    setCategoryList(state, categoryList) {
+      state.categoryList = categoryList
+    },
+  },
+
+  actions: {
+    getCategoryList({state, commit}) {
+      return new Promise((resolve, reject) => {
+        putCategoryList().then(response => {
+          const data = response.data
+          commit('setCategoryList', data)
+          resolve()
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    },
+    Addexpend ({ state, commit }, formItem) {
+      console.log(formItem)
+      return new Promise((resolve, reject) => {
+        Addexpend(formItem).then(JsonResponse => {
+          const data = JsonResponse.data
+          alert(data['info'])
+          resolve()
+        }).catch(err => {
+          reject(err)
+        })
+      })
+    }
+  }
+}

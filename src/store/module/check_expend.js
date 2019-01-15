4220e76afa7da83c2c2f36d055679cbf66b112a8
张敏 <841956630@qq.com>
2019-01-15 15:15:46 +0800

    Add add check_expend in module. Ref #129

---
+++ src/store/module/check_expend.js
@@ -0,0 +1,45 @@
+import {
+  putCategoryList,
+  putExpendInfo
+} from '../../api/check_expend'
+
+export default {
+  state: {
+    categoryList: [],
+    expendInfo: []
+  },
+  mutations: {
+    setCategoryList(state, categoryList) {
+      state.categoryList = categoryList
+    },
+    setExpendInfo(state, expendInfo) {
+      state.expendInfo = expendInfo
+
+    },
+    actions: {
+      getCategoryList({state, commit}) {
+        return new Promise((resolve, reject) => {
+          putCategoryList().then(response => {
+            const data = response.data
+            commit('setCategoryList', data)
+            resolve()
+          }).catch(error => {
+            reject(error)
+          })
+        })
+      },
+
+      listExpendInfo({state, commit}) {
+        return new Promise((resolve, reject) => {
+          putExpendInfo().then(response => {
+            const data = response.data
+            commit('setExpendInfo', data)
+            resolve(data)
+          }).catch(error => {
+            reject(error)
+          })
+        })
+      }
+    }
+  }
+}

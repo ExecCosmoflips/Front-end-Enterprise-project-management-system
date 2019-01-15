62b226bfb85da2b29294097251e80997d443d867
张敏 <841956630@qq.com>
2019-01-15 22:48:05 +0800

    Update update check_expend in API. Ref #140

--- src/api/check_expend.js
+++ src/api/check_expend.js
@@ -1,12 +1,21 @@
 import axios from '@/libs/api.request'
 
-export const putCategoryList = () => {
+export const putCategoryList = (project_id) => {
   return axios.request({
+    params: {
+      project_id
+    },
     url: '/get_category_list2',
     method: 'get'
   })
 }
 
+export const putProjectList = () => {
+  return axios.request({
+    url: '/get_project_list_by_id',
+    method: 'get'
+  })
+}
 
 
 export const putExpendInfo = () => {

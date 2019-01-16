d106b05e9b2c873a3fd08c91a936a46d5df2ac96
王怡然 <1452372625@qq.com>
2019-01-16 11:47:56 +0800

    Updata update index in module. Ref #143

--- src/store/module/receivable.js
+++ src/store/module/receivable.js
@@ -9,37 +9,36 @@ export default {
     receivableInfo: []
   },
   mutations: {
-    setCategoryList(state, categoryList) {
+    setCategoryList (state, categoryList) {
       state.categoryList = categoryList
     },
-    setReceivableInfo(state, receivableInfo) {
+    setReceivableInfo (state, receivableInfo) {
       state.receivableInfo = receivableInfo
-
+    }
+  },
+  actions: {
+    getCategoryList ({ state, commit }) {
+      return new Promise((resolve, reject) => {
+        putCategoryList().then(response => {
+          const data = response.data
+          commit('setCategoryList', data)
+          resolve()
+        }).catch(error => {
+          reject(error)
+        })
+      })
     },
-    actions: {
-      getCategoryList({state, commit}) {
-        return new Promise((resolve, reject) => {
-          putCategoryList().then(response => {
-            const data = response.data
-            commit('setCategoryList', data)
-            resolve()
-          }).catch(error => {
-            reject(error)
-          })
-        })
-      },
 
-      listReceivableInfo({state, commit}) {
-        return new Promise((resolve, reject) => {
-          putlistReceivableInfo().then(response => {
-            const data = response.data
-            commit('setReceivableInfo', data)
-            resolve(data)
-          }).catch(error => {
-            reject(error)
-          })
+    listReceivableInfo ({ state, commit }) {
+      return new Promise((resolve, reject) => {
+        putlistReceivableInfo().then(response => {
+          const data = response.data
+          commit('setReceivableInfo', data)
+          resolve(data)
+        }).catch(error => {
+          reject(error)
         })
-      }
+      })
     }
   }
 }

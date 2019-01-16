95215cc27a81fe3cbd82d5e78ec86f5df73d1953
张敏 <841956630@qq.com>
2019-01-16 20:33:14 +0800

    Update update project_personnel function in view. Ref #150

--- src/view/addreceivable/addreceivable.vue
+++ src/view/addreceivable/addreceivable.vue
@@ -1,49 +1,74 @@
 <template>
   <div>
     <Form :model="formItem" :rules="ruleValidate" :label-width="80" ref="formItem">
-
-      <FormItem label="选择项目" prop="project">
+      <Row>
+        <Col span="16" offset="4">
+      <FormItem label="选择项目" >
         <Select v-model="formItem.project" @on-change="getCategory(formItem.project)">
           <Option v-for=" item in projectList " :key="item.project_id" :value="item.project_id" > {{ item.project_name }} </Option>
         </Select>
       </FormItem>
-      <FormItem label="选择类别" prop="category">
+        </Col>
+      </Row>
+      <Row>
+        <Col span="16" offset="4">
+      <FormItem label="选择类别" >
         <Select v-model="formItem.category" >
           <Option  v-for=" item in categoryList " :key="item.category_id" :value="item.category_id" > {{ item.category_name }} </Option>
         </Select>
       </FormItem>
-
+        </Col>
+      </Row>
+      <Row>
+        <Col span="16" offset="4">
       <FormItem label="应收项标题" aria-setsize="10" prop="title">
         <Input v-model="formItem.title" placeholder="Enter something..."></Input>
       </FormItem>
+        </Col>
+      </Row>
+      <Row>
+        <Col span="16" offset="4">
       <FormItem label="应收数" prop="number">
         <Input v-model="formItem.number" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="Enter something..."></Input>
       </FormItem>
-
-      <FormItem prop="image">
-        <i-col span="16" offset="4">
-          <Card style="width:625px">
-            <div class="cropper-example cropper-first">
-              <cropper
-                :insideSrc="formItem.agreement"
-                :src="formItem.agreement"
-                crop-button-text="确认凭证"
-                @on-crop="handleCroped"
+        </Col>
+      </Row>
+      <Row>
+        <Col span="16" offset="3">
+           <FormItem prop="image">
+              <i-col span="16" offset="4">
+                <Card style="width:625px">
+                  <div class="cropper-example cropper-first">
+                     <cropper
+                        :insideSrc="formItem.agreement"
+                        :src="formItem.agreement"
+                         crop-button-text="确认凭证"
+                         @on-crop="handleCroped"
               ></cropper>
             </div>
           </Card>
         </i-col>
       </FormItem>
+        </Col>
+      </Row>
     </Form>
+    <Row>
+      <Col span="6" offset="10">
     <Button type="primary" @click=submit(formItem)>Submit</Button>
     <Button style="margin-left: 8px">Cancel</Button>
+      </Col>
+    </Row>
   </div>
 
 </template>
 <script>
   import {  mapState,mapActions } from 'vuex'
+  import Cropper from '@/components/cropper'
   export default {
     name: 'addreceivable',
+    components: {
+      Cropper
+    },
     data () {
       return {
         formItem: {
@@ -57,14 +82,8 @@
         },
         ruleValidate: {
 
-          project: [{
-            type: 'number', required: true, message: '请选择项目', trigger: 'change'
-          }],
-          category: [{
-            type: 'string', required: true, message: '请选择应收类别', trigger: 'change'
-          }],
           title: [{
-            type: 'string', required: true, message: '请选择应收款项', trigger: 'change'
+             required: true, message: '请选择应收款项', trigger: 'change'
           }],
           number: [{
             required: true, message: '请输入确认收入数', trigger: 'blur'
@@ -107,6 +126,7 @@
     },
     mounted() {
       this.getProjectList4()
+
 
     }
   }
